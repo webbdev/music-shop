029e2c820e27206acd048a0c92c4edf66e48ce04
webbdev <forrwebdev@gmail.com>
2025-04-12 13:02:36 +0100

    Update styles: adjust typography, spacing, and layout

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,8 +2,8 @@
 const about = {
 	heading: "About Us",
 	description: [
-		"We’re a team of passionate musicians helping our community make music. From quality gear to expert support, we’re here to keep you inspired. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin at cursus libero, tempus feugiat purus. Curabitur sagittis nulla quis nisi malesuada, eget efficitur massa varius.",
-		"Ut maximus convallis orci, sit amet facilisis risus auctor nec. Vestibulum diam augue, euismod sit amet auctor a, mattis et quam. Aenean congue mollis eros quis condimentum. Donec tincidunt congue dolor, id sagittis eros finibus id. Maecenas quis magna eu ex tristique interdum. Proin at cursus libero, tempus feugiat purus."
+		"We’re a team of passionate musicians helping our community make music. From quality gear to expert support, we’re here to keep you inspired. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin at cursus libero, tempus feugiat purus. Curabitur sagittis efficitur massa varius.",
+		"Ut maximus convallis orci, sit amet facilisis risus auctor nec. Vestibulum diam augue, euismod sit amet auctor a, mattis et quam. Aenean congue mollis eros quis condimentum. Donec tincidunt congue dolor, id sagittis eros finibus id. Proin at cursus libero, tempus feugiat purus."
 	]
 }
 
