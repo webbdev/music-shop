7887f2052658f234a64d4ce238c4ea0f0e5ec395
webbdev <forrwebdev@gmail.com>
2025-06-05 10:39:26 +0100

    update: use SVG icon for modal close button

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -39,7 +39,18 @@ const ImageModal: React.FC<ImageModalProps> = ({ src, alt, onClose }) => {
 					transition={{ duration: 0.3 }}
 					onClick={(e) => e.stopPropagation()}
 				>
-					<button className="modal-close" onClick={onClose} aria-label="Close modal">×</button>
+					<button className="modal-close" onClick={onClose} aria-label="Close modal">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							width="20"
+							height="20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path d="M18.3 5.71a1 1 0 0 0-1.41 0L12 10.59 7.11 5.7a1 1 0 0 0-1.41 1.41L10.59 12l-4.89 4.89a1 1 0 1 0 1.41 1.41L12 13.41l4.89 4.89a1 1 0 0 0 1.41-1.41L13.41 12l4.89-4.89a1 1 0 0 0 0-1.4z" />
+						</svg>
+					</button>
 					<img src={src} alt={alt} />
 				</motion.div>
 			</motion.div>
